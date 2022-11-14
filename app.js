6914a40938a21b6004b7f8db7e4108008b21ebad
Uzair Haroon <uharoon@live.co.uk>
2022-11-14 14:25:27 +0000

    Removed unnecessary error handling

--- app.js
+++ app.js
@@ -12,24 +12,6 @@ app.all("/*", (req, res) => {
     res.status(404).send({ msg: "Route not found" });
 });
 
-// PSQL Error Handling
-app.use((err, req, res, next) => {
-    if (err.code === "22P02") {
-        res.status(400).send({ msg: "Bad Request" });
-    } else {
-        next(err);
-    }
-});
-
-// Manual Error Handling
-app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.status(err.status).send({ msg: err.msg });
-    } else {
-        next(err);
-    }
-});
-
 // Server Erros?
 app.use((err, req, res, next) => {
     console.log(err);
