a092ccfa083f3a0be0ab3b9fe381a902968fc112
Uzair Haroon <uharoon@live.co.uk>
2022-11-15 11:20:16 +0000

    Implemented GET /api/reviews/:review_id

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ const express = require("express");
 const {
     getCategories,
     getReviews,
+    getReviewById,
 } = require("./controllers/games-controllers");
 
 const app = express();
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewById);
 
 app.all("/*", (req, res) => {
     res.status(404).send({ msg: "Route not found" });
+});
+
+// PSQL Error Handling
+app.use((err, req, res, next) => {
+    if (err.code === "22P02") {
+        res.status(400).send({ msg: "Bad Request" });
+    } else {
+        next(err);
+    }
+});
+
+// Manual Error Handling
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+    } else {
+        next(err);
+    }
 });
 
 // Server Erros?
--- models/games-model.js
+++ models/games-model.js
@@ -30,3 +30,24 @@ exports.selectReviews = () => {
             return result.rows;
         });
 };
+
+exports.selectReviewById = (review_id) => {
+    return db
+        .query(
+            `
+                SELECT *
+                FROM reviews
+                WHERE review_id = $1;
+            `,
+            [review_id]
+        )
+        .then((result) => {
+            if (result.rows.length === 0) {
+                return Promise.reject({
+                    status: 404,
+                    msg: `Review ID : ${review_id} does not exist`,
+                });
+            }
+            return result.rows[0];
+        });
+};
